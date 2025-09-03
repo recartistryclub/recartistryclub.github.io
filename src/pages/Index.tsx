08e13174142de7141f20e8495480112652983f77
dinesh-24 <dineshnanda.official@gmail.com>
2025-09-03 22:33:54 +0530

    photo path update3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Events from '../components/Events';
-import Gallery from '../components/Gallery';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import ScrollCanvas from '../components/ScrollCanvas';
@@ -23,7 +22,6 @@ const Index = () => {
         <About />
         <UpcomingEvents />
         <Events />
-        <Gallery />
         <Contact />
         <Footer />
       </div>
